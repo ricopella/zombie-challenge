377e4180977d6f9e9248486321b08e76e3a8624c
Narin Sundarabhaya <nrs710@gmail.com>
2017-07-08 09:40:58 -0700

    added colors and reformated log

--- zombiegame.js
+++ zombiegame.js
@@ -1,34 +1,18 @@
-// INSTRUCTIONS: Build a command-line based zombie fighting game. 
-// =========================================================================================================
+/* =========================================================================================================
 
-// In this game, you and a zombie will each be given a certain amount of health. (Perhaps: You 70, Zombie 15).
+In this game, you and a zombie will each be given a certain amount of health. (Perhaps: You 70, Zombie 15).
 
-// For each round, you will be asked to guess a random number between 1 and 5.
-// If your guess matches the random number of the Zombie -- you inflict a random amount of damage between 1 and 5. 
-// If your guess does not match the random number of the Zombie -- the Zombie inflicts a random amount of damage to you between 1 and 5.
-// Each round the zombie is given a new random number and you must guess again. 
+For each round, you will be asked to guess a random number between 1 and 5.
+If your guess matches the random number of the Zombie -- you inflict a random amount of damage between 1 and 5. 
+If your guess does not match the random number of the Zombie -- the Zombie inflicts a random amount of damage to you between 1 and 5.
+Each round the zombie is given a new random number and you must guess again. 
 
-// The game ends when you or the zombie gets to 0 health. 
+The game ends when you or the zombie gets to 0 health. 
 
-// Note: You should use the inquirer package to take in user commands.
-// Major Warning: inquirer's prompt function is "asynchronous", which means that the majority of your game logic will need to be inside the .then() function for your propmt. 
-
-/*
-
-zombie rolled (num)
-OH NO! The zombie slashed you with 5 damage
-You have 60 health left. The Zombie has 15 health left.
-
-or
-
-YOU HIT THE ZOMBIE WITH (num) damage
-You have (num) health left. The Zombie has (num) health left
-
- */
-
-// ===========================================================================================================
+ =========================================================================================================== */
 
 const inquirer = require('inquirer');
+const colors = require('colors');
 
 let gameData = {
     zombie: {
@@ -43,8 +27,8 @@ let gameData = {
 
 // generates random numbers & stores them in gameData object
 function getRandomNums() {
-    gameData.zombie.health = parseInt(Math.random() * (100 - 0) + 0);
-    gameData.user.health = parseInt(Math.random() * (100 - 0) + 0);
+    gameData.zombie.health = parseInt(Math.random() * (100 - 50) + 50);
+    gameData.user.health = parseInt(Math.random() * (100 - 50) + 50);
 } // end getRandomNums
 
 // checkResults
@@ -73,9 +57,9 @@ function eachRound() {
         gameCounter = gameCounter + 1;
     }
 
-    // test for health data
-    console.log(`zombie health: ${gameData.zombie.health}`);
-    console.log(`user health: ${gameData.user.health}`);
+    // // test for health data
+    // console.log(`zombie health: ${gameData.zombie.health}`);
+    // console.log(`user health: ${gameData.user.health}`);
 
     inquirer.prompt([{
         type: "list",
@@ -84,7 +68,7 @@ function eachRound() {
         name: "userGuess",
     }]).then(function(answers) {
         // Use user feedback for... whatever!! 
-        console.log(JSON.stringify(answers, null, '  '));
+        // console.log(JSON.stringify(answers, null, '  '));
 
         // generate random numbers each turn for zombie damage & user damage, convert to string
         gameData.zombie.damage = parseInt(Math.random() * (5 - 1) + 1).toString();
@@ -95,21 +79,20 @@ function eachRound() {
         if (answers.userGuess === gameData.zombie.damage) {
             // decrease zombie health by user.damage
             gameData.zombie.damage -= gameData.user.damage;
-            console.log(`HIT! Zomie Health: ${gameData.zombie.health} `);
-            console.log('Zombie Guess was: ' + gameData.zombie.damage);
+            console.log(`\nHIT! You hit the zombie with ${gameData.user.damage} damage.
+Your health is ${gameData.user.health} and Zombie health is ${gameData.zombie.health}\n`);
             // check health/round
             checkResults();
             // else if userGuess != zombie guess
         } else {
             // decrease user health by zombie.damage
             gameData.user.health -= gameData.zombie.damage;
-            console.log(`MISS! User Health: ${gameData.user.health} `);
-            console.log('Zombie Guess was: ' + gameData.zombie.damage);
+            console.log(`\nMISS! The Zombie Slashed you ${gameData.zombie.damage} damage. 
+Your health is ${gameData.user.health} and the Zombie health is ${gameData.zombie.health}\n`);
             // check health/round
             checkResults();
         }
     });
-
 
 } // end startGame
 
